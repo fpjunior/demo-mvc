c68e8029b2bffb96536393708d84e0d1e67a203e
fpjunior <fpjuniorpx@gmail.com>
2020-10-07 12:08:24 -0300

    crie departamento service

---
+++ src/main/java/com/fernando/curso/boot/service/DepartamentoServiceImpl.java
@@ -0,0 +1,50 @@
+package com.fernando.curso.boot.service;
+
+import java.util.List;
+
+import com.fernando.curso.boot.dao.DepartamentoDao;
+import com.fernando.curso.boot.domain.Departamento;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class DepartamentoServiceImpl implements DepartamentoService {
+
+    @Autowired
+    private DepartamentoDao dao;
+
+    @Override
+    @Transactional(readOnly = false)
+    public void salvar(Departamento departamento) {
+        dao.save(departamento);
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    public void editar(Departamento departamento) {
+        dao.update(departamento);
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    public void excluir(Long id) {
+        dao.delete(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Departamento buscarPorId(Long id) {
+        
+        return dao.findById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Departamento> buscarTodos() {
+        
+        return dao.findAll();
+    }
+
+}
